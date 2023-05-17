68d90628698559dc3cb26b830154151050f1bf89
Sarah Tanja <stanja@uw.edu>
2023-05-16 17:21:45 -0700

    blast is taking forever, can I use mox?

--- assignments/code/04-job.sh
+++ assignments/code/04-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-## Job Name
-#SBATCH --job-name=assign4
+## blast
+#SBATCH --job-name=blast
 ## Allocation Definition
 #SBATCH --account=srlab
 #SBATCH --partition=srlab
@@ -14,28 +14,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=stanja@uw.edu
 ## Specify the working directory for this job
-#SBATCH --chdir=/gscratch/scrubbed/stanja/assign_04
-
-# for rsync
-# run the following in the Rterminal, while connected to mox:
-# rsync -avz assignments/code/04-* stanja@mox.hyak.uw.edu:/gscratch/scrubbed/stanja/assign_04
-
-# for scp
-# run the following in the Rterminal, while connected to mox:
-# scp assignments/code/04-* stanja@mox.hyak.uw.edu:/gscratch/scrubbed/stanja/assign_04
-
-# type the following to schedule the job
-# sbatch 04-job-sh
-
-# Then on mox, inside the assign_04 directory you will type”
-
-# sbatch 04-job-sh to schedule the job.
-
-# Once done you should have a couple of new files in the directory.
-
-# You will want to check them to see if everything worked and then move the output back to your repo…
-
-# rsync -avz stanja@mox.hyak.uw.edu:/gscratch/scrubbed/stanja/assign_04/ 
+#SBATCH --chdir=/gscratch/scrubbed/stanja/blast
 
 #run a python script you wrote!
 module load intel-python3_2017
@@ -43,14 +22,14 @@ module load intel-python3_2017
 python 04-hello.py
 
 # run blastx just to get manual
-/gscratch/srlab/programs/ncbi-blast-2.10.1+/bin/blastx -help
+/gscratch/srlab/programs/ncbi-blast-2.10.1+/bin/blastx \
 
 #a few bash commands
 pwd 
 
 whoami
 
-echo "yeah! I did it!!!!!!!!!!"
+echo "yah! I did it!!!!!!!!!!"
 
 #this writes out  tofile
-echo "yeah! I did it!!!!!!!!!!" > text.file
+echo "yah! I did it!!!!!!!!!!" > text.file
